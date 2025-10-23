f07817e41a10568c5d68b5ea16c6ffaa3562c233
Abhishek Das <codecraftedbyabhi@gmail.com>
2025-10-24 00:57:28 +0530

    Update axios.js

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://my-portfolio-backend-murex.vercel.app/",
+  baseURL: "https://my-portfolio-backend-murex.vercel.app",
   withCredentials: true,
 });
 
