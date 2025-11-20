edc24fa538675754d6a8e96c9d92f0f3f21c0e20
Abhishek Das <codecraftedbyabhi@gmail.com>
2025-11-20 17:18:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://my-portfolio-admin-psi.vercel.app/",       
+  "https://my-portfolio-admin-psi.vercel.app/home",       
   "https://my-portfolio-user-frontend.vercel.app/" 
 ];
 
